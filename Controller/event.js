0eff557da6ea491235a2da6b59211ac632fa0ba4
ppatel <ppatel@mediaocean.com>
2022-12-06 03:41:45 +0530

    path update for event json

--- Controller/event.js
+++ Controller/event.js
@@ -1,7 +1,7 @@
 import { readFile } from 'fs/promises';
 
 //Reads the json file containing an array of events
-let events = JSON.parse(await readFile("./events.json", "utf8"));
+let events = JSON.parse(await readFile("../events.json", "utf8"));
 
 export const getRandomEvent = (req, res) => {
     //Gets a random value from events array.
